cc82a3d0225bed3e2cf7c8798a3afa8762ff4453
ropehapi <ropehapi@gmail.com>
2022-12-12 13:58:39 -0300

    Conectando e inserindo registros em BD MySQL

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"html/template"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-var temp = template.Must(template.ParseGlob("templates/*.html"));
+var (
+	db *sql.DB
+	err error
+	temp = template.Must(template.ParseGlob("templates/*.html"));
+)
+
 
 type Produto struct{
 	Nome, Descricao string
@@ -13,7 +22,35 @@ type Produto struct{
 	Quantidade int
 }
 
+func insertProduto(produto Produto) error{
+	fmt.Println("Chega até aqui")
+	_, err := db.Exec(fmt.Sprintf("INSERT INTO produto (nome, descricao, preco, quantidade) VALUES ('%s','%s',%f,%d)", produto.Nome, produto.Descricao, produto.Preco, produto.Quantidade))
+
+	if(err != nil){
+		return err
+	}
+
+	fmt.Println("Produto cadastrado com sucesso.")
+
+	return nil
+}
+
 func main(){
+
+	db, err = sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/loja_go")
+
+	produto := Produto{
+		"Camiseta",
+		"Confortável",
+		89.99,
+		15,
+	}
+
+	if inserterror := insertProduto(produto); inserterror != nil {
+		fmt.Println(inserterror)
+		panic(err)
+	}
+
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
 }
