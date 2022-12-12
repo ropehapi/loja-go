64b76226cab1e980913637a12c2848fa90ab64d0
ropehapi <ropehapi@gmail.com>
2022-12-12 01:39:33 -0300

    Retornando view com listagem dinâmica de produtos

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"html/template"
+	"net/http"
+)
+
+var temp = template.Must(template.ParseGlob("templates/*.html"));
+
+type Produto struct{
+	Nome, Descricao string
+	Preco float64
+	Quantidade int
+}
+
 func main(){
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8000", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request){
+	produtos := []Produto{
+		{"Camiseta","Confortável",180,15},
+		{"Tênis","12 molas",180,15},
+		{"Boné","Boné lagum",180,15},
+	}
 	
+	temp.ExecuteTemplate(w, "index", produtos)
 }
